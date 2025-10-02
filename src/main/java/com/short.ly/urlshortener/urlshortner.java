d6a00e4c20be50bb98c1e2ff394f5c165902d757
ASaha-os <akashsaha2268@gmail.com>
2025-10-02 13:44:45 +0530

    PR1 done ! Setup project structure and H2 DB

---
+++ src/main/java/com/short.ly/urlshortener/urlshortner.java
@@ -0,0 +1,12 @@
+// src/main/java/com/yourcompany/urlshortener/UrlShortenerApp.java
+
+// your company -> shortly 
+package com.shortly.urlshortener;
+
+public class UrlShortenerApp {
+
+    public static void main(String[] args) {
+        System.out.println("URL Shortener App Starting...");
+        // PR 7 will implement the main application loop here!
+    }
+}
